b8eeff6def045246d2fd9e89167dbed587b43d68
Aleksandr Kozhenkov <X8jDcFf1PKWHYDkh>
2020-10-22 17:12:39 +0300

    Get rid of keycloak dependency.

--- src/test/java/com/github/wirtzleg/networkbug/ExampleTest.java
+++ src/test/java/com/github/wirtzleg/networkbug/ExampleTest.java
@@ -1,11 +1,13 @@
 package com.github.wirtzleg.networkbug;
 
 import java.time.Duration;
-import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.ContainerLaunchException;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,11 +20,7 @@ public class ExampleTest {
     void shouldFail() {
         Network net = new NamedNetwork("z" + System.currentTimeMillis());
 
-        KeycloakContainer keycloak = new KeycloakContainer()
-            .withEnv("DB_VENDOR", "h2")
-            .withNetwork(net)
-            .withNetworkAliases("keycloak")
-            .withStartupTimeout(Duration.ofMinutes(1));
+        GenericContainer<?> keycloak = createKeycloakContainer(net);
 
         assertThrows(ContainerLaunchException.class, keycloak::start);
     }
@@ -31,12 +29,30 @@ public class ExampleTest {
     void shouldNotFail() {
         Network net = new NamedNetwork("a" + System.currentTimeMillis());
 
-        KeycloakContainer keycloak = new KeycloakContainer()
+        GenericContainer<?> keycloak = createKeycloakContainer(net);
+
+        keycloak.start();
+    }
+
+    private GenericContainer<?> createKeycloakContainer(Network net) {
+        GenericContainer<?> keycloak = new GenericContainer<>(DockerImageName.parse("quay.io/keycloak/keycloak:11.0.2"))
             .withEnv("DB_VENDOR", "h2")
             .withNetwork(net)
             .withNetworkAliases("keycloak")
-            .withStartupTimeout(Duration.ofMinutes(1));
+            .withExposedPorts(8080, 8443)
+            .withCommand(
+                "-c standalone.xml",
+                "-Dkeycloak.profile.feature.upload_scripts=enabled"
+            )
+            .withEnv("KEYCLOAK_USER", "admin")
+            .withEnv("KEYCLOAK_PASSWORD", "admin");
 
-        keycloak.start();
+        keycloak.setWaitStrategy(Wait
+            .forHttp("/auth")
+            .forPort(8080)
+            .withStartupTimeout(Duration.ofMinutes(1))
+        );
+
+        return keycloak;
     }
 }
